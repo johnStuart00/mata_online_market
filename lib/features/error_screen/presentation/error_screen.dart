7df0c19771984825db5254e9d2843a50f31492e2
John Stuart <ownworkplace01@gmail.com>
2025-02-26 13:09:56 +0500

    error page is done

--- lib/features/error_screen/presentation/error_screen.dart
+++ lib/features/error_screen/presentation/error_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 import 'package:mata_online_market/core/assets/app_images.dart';
+import 'package:mata_online_market/core/widgets/middle_text_widget.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class ErrorPage extends StatelessWidget {
   const ErrorPage({super.key});
@@ -15,17 +17,16 @@ class ErrorPage extends StatelessWidget {
           children: [
             Lottie.asset(AssetsPath().errorLottiePath),
             const SizedBox(height: 15),
-            const Text(
-              'Serwere baglanyp bilmedi. \nInternetiňizi barlaň!',
-              style: TextStyle(fontSize: 18),
-              textAlign: TextAlign.center,
-            ),
+            MiddleTextWidget(
+                text:
+                    '${AppLocalizations.of(context)!.serwereBaglanypBilmedi} \n ${AppLocalizations.of(context)!.internetiniziBarlan}'),
             const SizedBox(height: 20),
             ElevatedButton(
               onPressed: () {
                 Navigator.pop(context);
               },
-              child: const Text('Täzeden synanş'),
+              child: MiddleTextWidget(
+                  text: AppLocalizations.of(context)!.tazedenSynansyn),
             )
           ],
         ),
