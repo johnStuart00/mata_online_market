dd5132401151f16c1c82576d62f7b27658027dc4
John Stuart <ownworkplace01@gmail.com>
2025-03-07 12:37:31 +0500

    product screen almost done

--- lib/features/product_view_screen/presentation/product_view_screen.dart
+++ lib/features/product_view_screen/presentation/product_view_screen.dart
@@ -1,15 +1,15 @@
-import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:mata_online_market/config/routes/route_helper.dart';
 import 'package:mata_online_market/core/assets/app_icons.dart';
 import 'package:mata_online_market/core/constants/app_dimension.dart';
 import 'package:mata_online_market/core/constants/app_spacing.dart';
+import 'package:mata_online_market/core/widgets/product_container_widget.dart';
 import 'package:mata_online_market/core/widgets/text_widgets/big_text_widget.dart';
-import 'package:mata_online_market/core/widgets/icon_container_widget.dart';
 import 'package:mata_online_market/core/widgets/text_widgets/old_mark_text_widget.dart';
 import 'package:mata_online_market/core/widgets/text_widgets/middle_text_widget.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:mata_online_market/features/product_view_screen/widgets/product_image_carousel.dart';
 
 class ProductViewScreen extends StatefulWidget {
   const ProductViewScreen({super.key});
@@ -26,77 +26,133 @@ class _ProductViewScreenState extends State<ProductViewScreen> {
     return Scaffold(
       body: Padding(
         padding: AppSpacing.smallPadding,
-        child: CustomScrollView(slivers: [
-          //AppBar field start
-          SliverAppBar(
-            pinned: true,
-            centerTitle: true,
-            automaticallyImplyLeading: false,
-            title: MiddleTextWidget(
-              text: AppLocalizations.of(context)!.harytMaglumatlary,
-            ),
-            leading: GestureDetector(
-              onTap: () {
-                Get.back();
-              },
-              child: const Icon(AppIcons.arrowBack),
-            ),
-            actions: [
-              GestureDetector(
+        child: Stack(children: [
+          CustomScrollView(slivers: [
+            //AppBar field start
+            SliverAppBar(
+              pinned: true,
+              centerTitle: true,
+              automaticallyImplyLeading: false,
+              title: MiddleTextWidget(
+                text: AppLocalizations.of(context)!.harytMaglumatlary,
+              ),
+              leading: GestureDetector(
                 onTap: () {
                   Get.back();
                 },
-                child: const Icon(AppIcons.basket),
+                child: const Icon(AppIcons.arrowBack),
               ),
-              const SizedBox(width: 10),
-            ],
-          ),
-          //AppBar field end
-          //main field of the screen start
-          //image corousel start
-          const SliverToBoxAdapter(
-            child: SizedBox(
-              height: 300,
-              child: ImageCarouselScreen(),
-            ),
-          ),
-          const SliverToBoxAdapter(child: SizedBox(height: 10)),
-          //image corousel end
-          //product title start
-          const SliverToBoxAdapter(
-            child: BigTextWidget(text: 'Haryt ady'),
-          ),
-          const SliverToBoxAdapter(child: SizedBox(height: 10)),
-          const SliverToBoxAdapter(
-            child: _MukdaryField(),
-          ),
-          const SliverToBoxAdapter(child: SizedBox(height: 10)),
-          const SliverToBoxAdapter(
-            child: Row(
-              children: <Widget>[
-                OldMarkTextWidget(text: 'Köne baha'),
-                SizedBox(width: 3),
-                MiddleTextWidget(text: 'Täze baha'),
-              ],
-            ),
-          ),
-          const SliverToBoxAdapter(child: SizedBox(height: 10)),
-          const SliverToBoxAdapter(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                BigTextWidget(text: 'Beýany'),
-                SizedBox(height: 5),
-                MiddleTextWidget(
-                  text: 'Beýan barada giňişleýin maglumat ',
+              actions: [
+                GestureDetector(
+                  onTap: () {
+                    Get.back();
+                  },
+                  child: const Icon(AppIcons.basket),
                 ),
+                const SizedBox(width: 10),
               ],
             ),
-          )
-
-          //product title end
-          //main field of the screen end
+            //AppBar field end
+            //main field of the screen start
+            //image corousel start
+            const SliverToBoxAdapter(
+              child: SizedBox(
+                height: 300,
+                child: ProductImageCarousel(),
+              ),
+            ),
+            const SliverToBoxAdapter(child: SizedBox(height: 10)),
+            //image corousel end
+            //product title start
+            const SliverToBoxAdapter(
+              child: BigTextWidget(text: 'Haryt ady'),
+            ),
+            const SliverToBoxAdapter(child: SizedBox(height: 10)),
+            const SliverToBoxAdapter(
+              child: _MukdaryField(),
+            ),
+            const SliverToBoxAdapter(child: SizedBox(height: 10)),
+            const SliverToBoxAdapter(
+              child: Row(
+                children: <Widget>[
+                  OldMarkTextWidget(text: 'Köne baha'),
+                  SizedBox(width: 3),
+                  MiddleTextWidget(text: 'Täze baha'),
+                ],
+              ),
+            ),
+            const SliverToBoxAdapter(child: SizedBox(height: 10)),
+            const SliverToBoxAdapter(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  BigTextWidget(text: 'Beýany'),
+                  SizedBox(height: 5),
+                  SizedBox(
+                    height: 100.0,
+                    child: MiddleTextWidget(
+                      text: 'Beýan barada giňişleýin maglumat ',
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SliverToBoxAdapter(
+              child: BigTextWidget(text: 'Sizi gyzyklandyryp biler:'),
+            ),
+            SliverPadding(
+              padding: const EdgeInsets.all(10.0),
+              sliver: SliverGrid(
+                gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                  maxCrossAxisExtent: 300.0,
+                  mainAxisExtent: 245.0,
+                  crossAxisSpacing: 10.0,
+                  mainAxisSpacing: 10.0,
+                ),
+                delegate: SliverChildBuilderDelegate(
+                  (context, index) {
+                    return Padding(
+                      padding: AppSpacing.smallPadding,
+                      child: GestureDetector(
+                          onTap: () {}, child: const ProductContainerWidget()),
+                    );
+                  },
+                  childCount: 5,
+                ),
+              ),
+            ),
+            const SliverToBoxAdapter(child: SizedBox(height: 100)),
+            //product title end
+            //main field of the screen end
+          ]),
+          Positioned(
+              bottom: 10.0,
+              left: 0,
+              right: 0,
+              child: GestureDetector(
+                onTap: () {},
+                child: Container(
+                  height: 50,
+                  width: AppDimensions.screenWidth(context),
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      color: Theme.of(context)
+                          .bottomNavigationBarTheme
+                          .backgroundColor,
+                      borderRadius: AppSpacing.cardRadius,
+                      boxShadow: [
+                        BoxShadow(
+                          // ignore: deprecated_member_use
+                          color: Theme.of(context).shadowColor.withOpacity(0.3),
+                          blurRadius: 5.0,
+                          spreadRadius: 0.01,
+                          offset: const Offset(0, 2),
+                        ),
+                      ]),
+                  child: const MiddleTextWidget(text: 'Sebede goşmak'),
+                ),
+              ))
         ]),
       ),
     );
@@ -167,85 +223,6 @@ class _AppBarTitleFieldState extends State<_AppBarTitleField> {
         const SizedBox(width: 5),
         const SizedBox(width: 5),
       ],
-    );
-  }
-}
-
-class ImageCarouselScreen extends StatefulWidget {
-  const ImageCarouselScreen({super.key});
-
-  @override
-  State<ImageCarouselScreen> createState() => _ImageCarouselScreenState();
-}
-
-class _ImageCarouselScreenState extends State<ImageCarouselScreen> {
-  final List<String> imageUrls = [
-    'assets/images/1.jpg',
-    'assets/images/2.jpg',
-    'assets/images/3.jpg',
-    'assets/images/4.jpg',
-  ];
-
-  int _currentIndex = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Stack(
-          children: [
-            CarouselSlider(
-              options: CarouselOptions(
-                height: 300.0,
-                enlargeCenterPage: true,
-                aspectRatio: 16 / 9,
-                onPageChanged: (index, reason) {
-                  setState(() {
-                    _currentIndex = index;
-                  });
-                },
-              ),
-              items: imageUrls.map((url) {
-                return ClipRRect(
-                  borderRadius: AppSpacing.cardRadius,
-                  child: Image.asset(
-                    url,
-                    fit: BoxFit.cover,
-                    width: 1000.0,
-                  ),
-                );
-              }).toList(),
-            ),
-            Positioned(
-              bottom: 15.0,
-              left: 0,
-              right: 0,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: imageUrls.asMap().entries.map((entry) {
-                  return Container(
-                    width: 10.0,
-                    height: 10.0,
-                    margin: const EdgeInsets.symmetric(horizontal: 4.0),
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: _currentIndex == entry.key
-                          ? Theme.of(context).textTheme.bodyLarge!.color!
-                          // ignore: deprecated_member_use
-                          : Colors.grey.withOpacity(0.5),
-                    ),
-                  );
-                }).toList(),
-              ),
-            ),
-            Positioned(
-              bottom: 0.0,
-              right: AppDimensions.screenWidth(context) * 0.15,
-              child: const IconWidget(icon: AppIcons.unliked),
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
---
+++ lib/features/product_view_screen/widgets/product_image_carousel.dart
@@ -0,0 +1,85 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+import 'package:mata_online_market/core/assets/app_icons.dart';
+import 'package:mata_online_market/core/constants/app_dimension.dart';
+import 'package:mata_online_market/core/constants/app_spacing.dart';
+import 'package:mata_online_market/core/widgets/icon_container_widget.dart';
+
+class ProductImageCarousel extends StatefulWidget {
+  const ProductImageCarousel({super.key});
+
+  @override
+  State<ProductImageCarousel> createState() => _ProductImageCarouselState();
+}
+
+class _ProductImageCarouselState extends State<ProductImageCarousel> {
+  final List<String> imageUrls = [
+    'assets/images/1.jpg',
+    'assets/images/2.jpg',
+    'assets/images/3.jpg',
+    'assets/images/4.jpg',
+  ];
+
+  int _currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Stack(
+          children: [
+            CarouselSlider(
+              options: CarouselOptions(
+                height: 300.0,
+                enlargeCenterPage: true,
+                aspectRatio: 16 / 9,
+                onPageChanged: (index, reason) {
+                  setState(() {
+                    _currentIndex = index;
+                  });
+                },
+              ),
+              items: imageUrls.map((url) {
+                return ClipRRect(
+                  borderRadius: AppSpacing.cardRadius,
+                  child: Image.asset(
+                    url,
+                    fit: BoxFit.cover,
+                    width: 1000.0,
+                  ),
+                );
+              }).toList(),
+            ),
+            Positioned(
+              bottom: 15.0,
+              left: 0,
+              right: 0,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: imageUrls.asMap().entries.map((entry) {
+                  return Container(
+                    width: 10.0,
+                    height: 10.0,
+                    margin: const EdgeInsets.symmetric(horizontal: 4.0),
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: _currentIndex == entry.key
+                          ? Theme.of(context).textTheme.bodyLarge!.color!
+                          // ignore: deprecated_member_use
+                          : Colors.grey.withOpacity(0.5),
+                    ),
+                  );
+                }).toList(),
+              ),
+            ),
+            Positioned(
+              bottom: 0.0,
+              right: AppDimensions.screenWidth(context) * 0.15,
+              child: const IconWidget(icon: AppIcons.unliked),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
